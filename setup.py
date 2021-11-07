76e15cc66e7209320b2b0557f1c27e3e0b549c67
Ben Chambule <benchambule@gmail.com>
2021-11-07 18:06:55 +0200

    Updating lottus version on setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lottus",
-    version="0.0.2",
+    version="0.0.3",
     author="Benjamim Chambule",
     author_email="benchambule@gmail.com",
     description="An ussd library that will save you time",
